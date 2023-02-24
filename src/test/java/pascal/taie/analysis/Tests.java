ec236b1d3bdfb6ed2c480a195f03c26ac6c00ab2
Chuyang Chen <chuyangchen2018@outlook.com>
2023-02-24 21:07:12 +0800

    tmp

--- src/test/java/pascal/taie/analysis/Tests.java
+++ src/test/java/pascal/taie/analysis/Tests.java
@@ -47,7 +47,7 @@ public final class Tests {
     /**
      * Whether generate expected results or not.
      */
-    private static final boolean GENERATE_EXPECTED_RESULTS = false;
+    private static final boolean GENERATE_EXPECTED_RESULTS = true;
 
     /**
      * Whether dump control-flow graphs or not.
--- src/test/java/pascal/taie/analysis/pta/SolarTest.java
+++ src/test/java/pascal/taie/analysis/pta/SolarTest.java
@@ -12,4 +12,90 @@ public class SolarTest {
     public void testBasicEntry() {
         Tests.testPTA(DIR, "Basic", OPTS);
     }
+
+    @Test
+    public void testArgsRefineEntry() {
+        Tests.testPTA(DIR, "ArgsRefine", OPTS);
+    }
+
+    @Test
+    public void testBasicFieldEntry() {
+        Tests.testPTA(DIR, "BasicField", OPTS);
+    }
+
+    @Test
+    public void testCastRecvEntry() {
+        Tests.testPTA(DIR, "CastRecv", OPTS);
+    }
+
+    @Test
+    public void testDummyCastEntry() {
+        Tests.testPTA(DIR, "DummyCast", OPTS);
+    }
+
+    @Test
+    public void testDuplicateNameEntry() {
+        Tests.testPTA(DIR, "DuplicateName", OPTS);
+    }
+
+    @Test
+    public void testFieldS2TEntry() {
+        Tests.testPTA(DIR, "FieldS2T", OPTS);
+    }
+
+    @Test
+    public void testFieldSigEntry() {
+        Tests.testPTA(DIR, "FieldSig", OPTS);
+    }
+
+    @Test
+    public void testFieldTpEntry() {
+        Tests.testPTA(DIR, "FieldTp", OPTS);
+    }
+
+    @Test
+    public void testGetMethodsEntry() {
+        Tests.testPTA(DIR, "GetMethods", OPTS);
+    }
+
+    @Test
+    public void testInheritanceEntry() {
+        Tests.testPTA(DIR, "Inheritance", OPTS);
+    }
+
+    @Test
+    public void testKnownRecvEntry() {
+        Tests.testPTA(DIR, "KnownRecv", OPTS);
+    }
+
+    @Test
+    public void testLazyHeapFieldEntry() {
+        Tests.testPTA(DIR, "LazyHeapField", OPTS);
+    }
+
+    @Test
+    public void testMethodInferenceEntry() {
+        Tests.testPTA(DIR, "MethodInference", OPTS);
+    }
+
+    @Test
+    public void testRecvTypeEntry() {
+        Tests.testPTA(DIR, "RecvType", OPTS);
+    }
+
+    @Test
+    public void testUnknownClassNameEntry() {
+        Tests.testPTA(DIR, "UnknownClassName", OPTS);
+    }
+
+    @Test
+    public void testUnknownMethodNameEntry() {
+        Tests.testPTA(DIR, "UnknownMethodName", OPTS);
+    }
+
+    @Test
+    public void testUnknownRecvEntry() {
+        Tests.testPTA(DIR, "UnknownRecv", OPTS);
+    }
+
 }
