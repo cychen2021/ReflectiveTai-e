420cbb025c502802f8c8c7b97da02d5339f520ac
Chuyang Chen <chuyangchen2018@outlook.com>
2023-02-25 15:03:06 +0800

    Add lightweight annotations
    The actual logic isn't implemented due to Tai-e's limitation. It will be added after Tai-e is updated.

---
+++ src/main/java/pascal/taie/analysis/pta/plugin/solar/annotations/ReflectiveField.java
@@ -0,0 +1,14 @@
+package pascal.taie.analysis.pta.plugin.solar.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// TODO: Due to limitation of Tai-e framework, currently unsupported. Relevant function will be
+//  implemented after Tai-e framework is updated.
+@Target(ElementType.LOCAL_VARIABLE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ReflectiveField {
+    public String[] candidates() default {};
+}
